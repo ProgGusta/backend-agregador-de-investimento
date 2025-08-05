a45b3bc0c389e517125309a658f5d510ec089fcd
ProgGusta <gustavoprogpc@gmail.com>
2025-08-05 15:35:52 -0300

    new Entity account

---
+++ agregadorinvestimento/src/main/java/tech/project/agregadorinvestimento/entity/Account.java
@@ -0,0 +1,31 @@
+package tech.project.agregadorinvestimento.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_accounts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String description;
+}
