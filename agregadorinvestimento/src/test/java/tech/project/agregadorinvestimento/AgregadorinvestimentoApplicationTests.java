c463b6e24b7b0c8dfda6e7f1613b651e81adc079
ProgGusta <gustavoprogpc@gmail.com>
2025-08-05 11:33:54 -0300

    first commit

---
+++ agregadorinvestimento/src/test/java/tech/project/agregadorinvestimento/AgregadorinvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.project.agregadorinvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorinvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
